64cc429e831cd8fe244bba2c8b920ee5ebd0fec3
krzykamil <krzysiekkamilpiotrek@gmail.com>
2023-11-22 16:10:54 +0100

    Init commit

---
+++ slices/main/actions/home/show.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Main
+  module Actions
+    module Home
+      class Show < Main::Action
+        def handle(*, response)
+          response.render(view)
+        end
+      end
+    end
+  end
+end
---
+++ slices/main/config/routes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Main
+  class Routes < Hanami::Routes
+    root to: 'home.show'
+  end
+end
