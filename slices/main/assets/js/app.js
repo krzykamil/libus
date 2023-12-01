2713940480ea94dd8dae996d7fc21583898724ed
krzykamil <krzysiekkamilpiotrek@gmail.com>
2023-12-01 11:55:29 +0100

    Theme switcher

--- slices/main/assets/js/app.js
+++ slices/main/assets/js/app.js
@@ -1,5 +1,30 @@
-import "../builds/tailwind.css"
+import "../builds/tailwind.css";
 import "../css/app.css";
 import 'htmx.org';
-import './htmx.js'
-console.log('hanami entrypoint')
+import './htmx.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeController = document.querySelector('.theme-controller');
+    const htmlTag = document.documentElement;
+
+    console.log(themeController)
+    function toggleTheme() {
+        if (themeController.checked) {
+            htmlTag.setAttribute('data-theme', 'nord');
+            localStorage.setItem('theme', 'nord');
+        } else {
+            htmlTag.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    }
+
+    // Set initial theme based on local storage
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        htmlTag.setAttribute('data-theme', savedTheme);
+        themeController.checked = savedTheme === 'nord';
+    }
+
+    // Add onclick event dynamically
+    themeController.addEventListener('click', toggleTheme);
+});
