e47bce7cd3732492ab6584456857f232ca26db08
mrimp <krzysiekkamilpiotrek@gmail.com>
2024-04-25 14:47:34 +0200

    some cleanup

--- slices/main/actions/books/update.rb
+++ slices/main/actions/books/update.rb
@@ -1,27 +1,16 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 module Main
   module Actions
     module Books
       class Update < Main::Action
         include Deps['persistence.rom', books_repo: 'repositories.books']
-        # params do
-        #   required(:id).filled(:integer)
-        #   required(:book).schema do
-        #   #   optional(:title).value(:string)
-        #   #   optional(:author_id).value(:string)
-        #   #   optional(:published_date).value(:date)
-        #   #   optional(:description).value(:string)
-        #   #   optional(:image_url).value(:string)
-        #     optional(:image).value(:hash)
-        #   #   optional(:category).value(:string)
-        #   #   optional(:language).value(:string)
-        #   #   optional(:isbn_10).value(:string)
-        #   #   optional(:isbn_13).value(:string)
-        #   end
-        # end
+        params do
+          required(:id).filled(:integer)
+          required(:book).schema do
+            optional(:image).value(:hash)
+          end
+        end
 
         def handle(request, response)
           halt 422, {errors: request.params.errors}.to_json unless request.params.valid?
--- slices/main/actions/search_progress/show.rb
+++ slices/main/actions/search_progress/show.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 module Main
   module Actions
     module SearchProgress
