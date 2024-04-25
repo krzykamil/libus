dba2f7faed3e2e695d87aacc60298c6763c43482
mrimp <krzysiekkamilpiotrek@gmail.com>
2024-04-25 14:59:58 +0200

    base spec

---
+++ spec/requests/image_upload_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'pry'
+RSpec.describe 'ImageUploadSpec', :db, type: :request do
+  context 'when photo is uploaded' do
+    context "when user is logged in" do
+      let!(:user) { factory[:user, name: "Guy", email: "my@guy.com"] }
+      let!(:book) { factory[:book] }
+
+      it 'changes the photo' do
+        login_as user
+        patch "/books/#{book.id}", { id: book.id, book: { image: Rack::Test::UploadedFile.new("spec/fixtures/image.png") } }
+        expect(JSON.parse(rom.relations[:books].first.image_data)["id"]).to be_a(String)
+
+        expect(last_response.status).to be(302)
+      end
+    end
+  end
+end
